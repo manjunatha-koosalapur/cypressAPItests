9d4c4923e0ac0bee4f53646b42603f1116e6242d
manjunatha-koosalapur <manjunatha.koosalapur@gmail,com>
2022-02-06 19:46:42 +0530

    1cmt

---
+++ cypress/integration/testSuite/ts01-GETuser.js
@@ -0,0 +1,40 @@
+///<reference types = "Cypress" />
+
+describe('GET user', ()=>{
+    
+    let accessToken = 'c7f0aaeacaf27a1e9d7bf666d905e49e170c53f19735ded8b0d8e2cbfa5d85fc' //we can call this in tests
+
+
+    it('test01: GET all users', ()=>{
+    cy.request({
+        method: 'GET',
+        url: 'https://gorest.co.in/public-api/users/',
+        headers: {
+            'authorization': "Bearer " + accessToken
+        }
+    }).then((response)=>{
+        expect(response.status).to.eq(200)
+        // assert.equal(response.status, 200)       //we can use this also for assertion step
+        expect(response.body.data).to.be.a('array')
+        expect(response.body).to.not.be.null
+        // expect(response.body).to.be.null         //intenstionally to fail the assertion
+        // console.log(response)
+        })
+    })
+
+    it('test02: GET user by ID', ()=>{
+        cy.request({
+            method: 'GET',
+            url: 'https://gorest.co.in/public-api/users/3747',
+            headers: {
+                'authorization': "Bearer " + accessToken
+            }
+        }).then((response)=>{
+            expect(response.status).to.eq(200)
+            expect(response.body).to.not.be.null
+            expect(response.body.data.id).to.eq(3747)
+            expect(response.body.data.name).to.eq('Uttam Nayar')
+            })
+        })
+
+})
