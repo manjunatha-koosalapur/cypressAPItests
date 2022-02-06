5b267b95163769a78128e215dc7979ebbea5ffd1
manjunatha-koosalapur <manjunatha.koosalapur@gmail,com>
2022-02-06 22:09:10 +0530

    cmt4

--- cypress/integration/testSuite/ts02-POSTuser.js
+++ cypress/integration/testSuite/ts02-POSTuser.js
@@ -4,7 +4,6 @@ describe('POST user request', ()=>{
     
     let accessToken = 'c7f0aaeacaf27a1e9d7bf666d905e49e170c53f19735ded8b0d8e2cbfa5d85fc' //we can call this in tests
 
-
     it('test01: Create user', ()=>{
     cy.request({
         method: 'POST',
@@ -13,8 +12,8 @@ describe('POST user request', ()=>{
             'authorization': "Bearer " + accessToken
         },
         body: {
-            "name": "Manjunath Cypress API Test6",
-            "email": "cypressAPI6@testEmail.com",
+            "name": "Manjunath Cypress API Test",
+            "email": "cypressAPI11@testEmail.com",
             "gender": "male",
             "status": "active"
         }
@@ -22,11 +21,96 @@ describe('POST user request', ()=>{
     }).then((response)=>{
         expect(response.status).to.eq(200)
         // assert.equal(response.status, 200)       //we can use this also for assertion step
-        expect(response.body.data).has.property('email','cypressAPI6@testEmail.com')
-        expect(response.body.data).has.property('name','Manjunath Cypress API Test6')
         expect(response.body).to.not.be.null
+        expect(response.body.data).has.property('email','cypressAPI11@testEmail.com')
+        expect(response.body.data).has.property('name','Manjunath Cypress API Test')
+        expect(response.body.data).has.property('gender','male')
+        expect(response.body.data).has.property('status','active')
         // expect(response.body).to.be.null         //intenstionally to fail the assertion
         // console.log(response)
+        // cy.log(JSON.stringify(response))
+        })
+    })
+
+})
+
+//Randomizing User Name and User Email ID
+describe('POST user request - Random User Name & Email', ()=>{
+    
+    let accessToken = 'c7f0aaeacaf27a1e9d7bf666d905e49e170c53f19735ded8b0d8e2cbfa5d85fc' //we can call this in tests
+    //to genearte random mail id
+    let randomText = ""
+    let testEmail = ""
+    let testName = ""
+
+    it('test02: Create user', ()=>{
+        var pattern = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+        for (var i = 0; i < 10; i++)
+        randomText+=pattern.charAt(Math.floor(Math.random() * pattern.length));
+        testEmail = randomText + '@gmail.com'
+        testName = randomText
+
+        cy.request({
+        method: 'POST',
+        url: 'https://gorest.co.in/public-api/users/',
+        headers: {
+            'authorization': "Bearer " + accessToken
+        },
+        body: {
+            "name": testName,
+            "email": testEmail,
+            "gender": "male",
+            "status": "active"
+        }
+
+    }).then((response)=>{
+        expect(response.status).to.eq(200)
+        // assert.equal(response.status, 200)       //we can use this also for assertion step
+        expect(response.body).to.not.be.null
+        expect(response.body.data).has.property('email',testEmail)
+        expect(response.body.data).has.property('name',testName)
+        expect(response.body.data).has.property('gender','male')
+        expect(response.body.data).has.property('status','active')
+        // expect(response.body).to.be.null         //intenstionally to fail the assertion
+        // console.log(response)
+        cy.log(JSON.stringify(response))
+        })
+    })
+
+})
+
+//passing test data from fixture file
+const testData = require('../../fixtures/createUser')
+
+describe('POST user request- testData from fixture file', ()=>{
+    
+    let accessToken = 'c7f0aaeacaf27a1e9d7bf666d905e49e170c53f19735ded8b0d8e2cbfa5d85fc' //we can call this in tests
+
+    it('test03: Create user', ()=>{
+    cy.request({
+        method: 'POST',
+        url: 'https://gorest.co.in/public-api/users/',
+        headers: {
+            'authorization': "Bearer " + accessToken
+        },
+        body: {
+            "name": testData.name,
+            "email": testData.email,
+            "gender": testData.gender,
+            "status": testData.status
+        }
+
+    }).then((response)=>{
+        expect(response.status).to.eq(200)
+        // assert.equal(response.status, 200)       //we can use this also for assertion step
+        expect(response.body).to.not.be.null
+        expect(response.body.data).has.property('email',testData.email)
+        expect(response.body.data).has.property('name',testData.name)
+        expect(response.body.data).has.property('gender',testData.gender)
+        expect(response.body.data).has.property('status',testData.status)
+        // expect(response.body).to.be.null         //intenstionally to fail the assertion
+        // console.log(response)
+        // cy.log(JSON.stringify(response))
         })
     })
 
