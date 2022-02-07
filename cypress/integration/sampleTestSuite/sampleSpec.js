ad51049563367a6ecedc63ff9b8f33a7ef69440d
manjunatha-koosalapur <manjunatha.koosalapur@gmail,com>
2022-02-08 01:11:01 +0530

    cmt0208

---
+++ cypress/integration/sampleTestSuite/sampleSpec.js
@@ -0,0 +1,19 @@
+describe('describe block <set of test scripts>', ()=>{
+    
+       it('test01: Login', ()=>{
+
+       })
+
+       it('test02: Validate logged in user', ()=>{
+
+       })
+
+       it('test03: Change the password', ()=>{
+
+        })
+
+        it('test03: Logout', ()=>{
+
+        })
+
+})
